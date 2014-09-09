98851840e6c47a2c18cf6da46180ea4584a86174
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2014-09-10 00:12:49 +0200

    rudimentary replay viewer

---
+++ replay_utils/replay_viewer.py
@@ -0,0 +1,43 @@
+import glob
+import os
+import re
+
+import flask
+import jinja2
+
+import recorder
+
+app = flask.Flask(__name__)
+
+template_loader = jinja2.FileSystemLoader('templates')
+app.jinja_loader = template_loader
+
+
+# Custom static data
+@app.route('/replay_data/<path:filename>')
+def custom_static(filename):
+    res = flask.send_from_directory(recorder.REPLAY_DIR, filename)
+    print(res)
+    return res
+
+
+@app.route('/')
+def index():
+    frames = set()
+    files = glob.glob(os.path.join(recorder.REPLAY_DIR, '*'))
+    for filename in files:
+        base, ext = os.path.splitext(os.path.basename(filename))
+        m = re.match(r'[a-z_]*(\d+)$', base)
+        frames.add(m.group(1))
+    frames = sorted(frames)
+
+    return flask.render_template('main.html', **locals())
+
+
+def main():
+    app.debug = True
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ replay_utils/static/browse.js
@@ -0,0 +1,45 @@
+function selectFrame(frame) {
+    console.log(frame);
+    var img_path =
+        '/replay_data/hz' + frame + '.png?no_cache=' + Math.random();
+    $('#pic').attr('src', img_path);
+
+    $('#frame' + selected).removeClass('selected');
+    selected = frame;
+    $('#frame' + frame).addClass('selected');
+
+    window.location.hash = frame;
+}
+
+if (window.location.hash) {
+    selectFrame(window.location.hash.substring(1));
+}
+
+
+function keydown(e) {
+    var keyCode = e.keyCode || e.which;
+
+    var i = frames.indexOf(selected);
+
+    if (keyCode == 40) {
+        // up
+        if (i + 1 < frames.length)
+            selectFrame(frames[i + 1]);
+    } else if (keyCode == 38) {
+        // down
+        if (i > 0)
+            selectFrame(frames[i - 1]);
+    } else if (keyCode == 36) {
+        // home
+        selectFrame(frames[0]);
+    } else if (keyCode == 35) {
+        // end
+        selectFrame(frames[frames.length - 1]);
+    } else {
+        return;
+    }
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+$('*').keydown(keydown);
